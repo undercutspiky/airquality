a93a7d1885325d0faf1c955a1114d1b53aaf15ee
tomard <tomard@usi.ch>
2016-05-09 15:07:19 +0200

    display values retieved

--- routes/readData.js
+++ routes/readData.js
@@ -7,18 +7,19 @@ var monthAgo = new Date(Date.now());
 monthAgo.setMonth(monthAgo.getMonth() - 1);
 
 router.get('/showData',function(req,res){
+	var response = "Values found are ";
 	Sensor.find().where('timeStamp').gt(monthAgo).exec(function(err, values) {
 		if (err) throw err;
 
 		// show the admins in the past month
-		console.log(values);
+		//console.log(values);
 		for(var i in values){
 			console.log(values[i].value);
+			response += ' '+ values[i].value.toString();
 		}
+		console.log(response);
+		res.send(response);
 	});
-
-	res.send('Values found');
-
 });
 
 module.exports = router;
